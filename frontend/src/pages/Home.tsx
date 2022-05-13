e52bf427e0c0b4f13e5c17cec94867392b8b050b
Manuel <manutepowa@gmail.com>
2022-05-13 11:22:09 +0200

    Fix regex for spaces on enter room and fix duplicate variable audioSetting

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -18,7 +18,7 @@ function Home () {
         <form className="!appearance-none my-12 " onSubmit={acceder}>
           <span className='flex flex-row items-end'>
             <button className="w-4 h-[48px] rounded-tl-md rounded-bl-md pl-3 pr-4 bg-indigo-500 shadow-lg appearance-none text-stone-50 font-bold fcapitana tracking-wide">/</button>
-            <input type="text" value={path} onChange={(e) => setPath(e.target.value.replace(/\//g, ''))} size={60} placeholder="Nombre de la sala" className="h-[48px] px-3 -mt-2 text-sm font-medium placeholder-opacity-50 transition duration-200  border-opacity-50 shadow-xl outline-none sm:px-2 bg-stone-50 text-stone-400 placeholder-stone-800 focus:placeholder-emerald-300 focus:border-emerald-400" />
+            <input type="text" value={path} onChange={(e) => setPath(e.target.value.replace(/[\\/ ]/g, ''))} size={60} placeholder="Nombre de la sala" className="h-[48px] px-3 -mt-2 text-sm font-medium placeholder-opacity-50 transition duration-200  border-opacity-50 shadow-xl outline-none sm:px-2 bg-stone-50 text-stone-400 placeholder-stone-800 focus:placeholder-emerald-300 focus:border-emerald-400" />
             <button className="h-[48px] px-6 bg-indigo-500 rounded-tr-lg rounded-br-lg shadow-xl appearance-none hover:bg-indigo-700 text-stone-100 fcapitana tracking-wide font-semibold" type='submit'>acceder</button>
           </span>
         </form>
